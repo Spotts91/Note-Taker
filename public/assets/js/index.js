84937a859bc506fe5a46c1ed3dac8047112c61fb
Spotts91 <oakshassa864@gmail.com>
2022-02-09 16:19:14 -0800

    starter code and beginnings of JS coding

---
+++ public/assets/js/index.js
@@ -0,0 +1,65 @@
+var $noteTitle = $(".note-title");
+var $noteText = $(".note-textarea");
+var $saveNoteBtn = $(".save-note");
+var $newNoteBtn = $(".new-note");
+var $noteList = $(".list-container .list-group");
+
+// activeNote is used to keep track of the note in the textarea
+var activeNote = {};
+
+// A function for getting all the notes from the db
+var getNotes = function() {
+  return $.ajax({
+    url: "/api/notes",
+    data: note,
+    method: "GET"
+  });
+};
+
+// A function for saving a note to the db
+var saveNote = function(note) {
+  return $.ajax({
+    url: "/api/notes",
+    method: "GET"
+  });
+};
+
+// A function for deleting a note from the db
+var deleteNote = function(id) {
+  return $.ajax({
+    url: "api/notes" + id,
+    method: "DELETE"
+  });
+};
+
+// If there is an activeNote, display it, otherwise render empty inputs
+var renderActiveNote = function() {
+  $saveNoteBtn.hide();
+
+  if (typeof activeNote.id === "number") {
+    $noteTitle.attr("readonly", true);
+    $noteText.attr("readonly", true);
+    $noteTitle.val(activeNote.title);
+    $noteText.val(activeNote.text);
+  } else {
+    $noteTitle.attr("readonly", false);
+    $noteText.attr("readonly", false);
+    $noteTitle.val("");
+    $noteText.val("");
+  }
+};
+
+// Get the note data from the inputs, save it to the db and update the view
+var handleNoteSave = function() {
+  var newNote = {
+    title: $noteTitle.val(),
+    text: $noteTextx.val()
+  };
+
+  saveNote(newNote);
+    getAndRenderNotes();
+    renderActiveNote();
+};
+
+// Delete the clicked note
+
